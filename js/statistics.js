7f59aec5a67edc3fa2c5245812698638f4aa20a4
Anderson Lira <anderson.lira@celfocus.com>
2019-09-13 09:57:01 +0100

    Statistics ok

--- js/statistics.js
+++ js/statistics.js
@@ -23,15 +23,25 @@ const ordered = data.sort(function(a,b){
     return a.label - b.label;
 });
 console.log(ordered);
+labels = [];
+data = [];
+for(var i in ordered){
+    var obj = ordered[i];
+    var key = obj.label.getDate() + "-" + (obj.label.getMonth()+1) + "-" + obj.label.getFullYear();
+    labels.push(key);
+
+    data.push(parseInt(obj.value));
+
+}
 
 
 var myChart = new Chart(ctx, {
     type: 'line',
     data: {
-        labels: ["Tokyo",	"Mumbai",	"Mexico City",	"Shanghai",	"Sao Paulo",	"New York",	"Karachi","Buenos Aires",	"Delhi","Moscow"],
+        labels: labels,
         datasets: [{
-            label: 'Series 1', // Name the series
-            data: [500,	50,	2424,	14040,	14141,	4111,	4544,	47,	5555, 6811], // Specify the data values array
+            label: 'Desempenho', // Name the series
+            data: data, // Specify the data values array
             fill: false,
             borderColor: '#2196f3', // Add custom color border (Line)
             backgroundColor: '#2196f3', // Add custom color background (Points and Fill)
