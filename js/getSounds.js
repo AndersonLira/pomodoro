de7cb227001f45dabe593aa2f386ca6a09f52511
Anderson Lira <anderson.lira@celfocus.com>
2020-07-13 11:51:01 +0100

    Viva Sena mp3 added

--- js/getSounds.js
+++ js/getSounds.js
@@ -4,5 +4,6 @@ function getSounds(){
         new Sound({id: 2, title:"Submarino",source:"sounds/submarine.mp3"}),
         new Sound({id: 3, title:"Sino",source:"sounds/bell.mp3"}),
         new Sound({id: 4, title:"Campainha",source:"sounds/doorBell.mp3"}),
+        new Sound({id: 5, title:"Viva Sena",source:"sounds/vivaSena.mp3"}),
     ];
 }
